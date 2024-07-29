1d4f6975055c0d20bc7a413238fe2bcf8d1e7028
clemetayer <c.lemetayer@laposte.net>
2024-07-29 22:06:19 +0200

    first draft of a scale adapting camera + added a temporary CPU player implementation

--- Scenes/Camera/camera.gd
+++ Scenes/Camera/camera.gd
@@ -9,7 +9,7 @@ extends Camera2D
 
 ##### VARIABLES #####
 #---- CONSTANTS -----
-# const constant = 10 # Optionnal comment
+const ZOOM_OFFSET := Vector2i.ONE * 1000
 
 #---- EXPORTS -----
 @export var PLAYERS : Array[NodePath]
@@ -36,6 +36,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame. Remove the "_" to use it.
 func _process(_delta):
 	global_position = _get_average_position()
+	zoom = Vector2.ONE * _get_best_zoom()
+	DebugInterface.set_debug_text("Best zoom", _get_best_zoom())
 
 ##### PUBLIC METHODS #####
 # Methods that are intended to be "visible" to other nodes or scripts
@@ -48,6 +50,37 @@ func _get_average_position() -> Vector2:
 	for player in PLAYERS:
 		sum += get_node(player).global_position
 	return sum / PLAYERS.size()
+
+func _get_best_zoom() -> float:
+	var min_max_pos = _get_global_min_max_pos()
+	DebugInterface.set_debug_text("Zoom min max pos", min_max_pos)
+	var min_pos = min_max_pos.min
+	var max_pos = min_max_pos.max
+	var screen_size_offset = DisplayServer.screen_get_size() - ZOOM_OFFSET
+	DebugInterface.set_debug_text("Screen size offset", screen_size_offset)
+	var best_zoom := Vector2.ZERO
+	DebugInterface.set_debug_text("Pos diff", Vector2(min_pos.x - max_pos.x, min_pos.y - max_pos.y))
+	best_zoom.x = max(abs(min_pos.x - max_pos.x)/screen_size_offset.x,1)
+	best_zoom.y = max(abs(min_pos.y - max_pos.y)/screen_size_offset.y,1)
+	return 1/max(best_zoom.x, best_zoom.y)
+
+func _get_global_min_max_pos() -> Dictionary:
+	var min := Vector2.INF
+	var max := Vector2.ZERO
+	for player_path in PLAYERS:
+		var player = get_node(player_path)
+		if player.global_position.x < min.x:
+			min.x = player.global_position.x
+		if player.global_position.y < min.y:
+			min.y = player.global_position.y
+		if player.global_position.x > max.x:
+			max.x = player.global_position.x
+		if player.global_position.y > max.y:
+			max.y = player.global_position.y
+	return {
+		"min": min,
+		"max": max
+	}
 
 ##### SIGNAL MANAGEMENT #####
 # Functions that should be triggered when a specific signal is received
