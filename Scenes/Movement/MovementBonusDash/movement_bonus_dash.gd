349c2b83b5c6f4688aa13742e29642b2dc32a417
clemetayer <c.lemetayer@laposte.net>
2024-08-05 21:42:32 +0200

    added an amount of dashes and a reload time

--- Scenes/Movement/MovementBonusDash/movement_bonus_dash.gd
+++ Scenes/Movement/MovementBonusDash/movement_bonus_dash.gd
@@ -10,6 +10,7 @@ extends MovementBonusBase
 ##### VARIABLES #####
 #---- CONSTANTS -----
 const DASH_VELOCITY := 1500
+const MAX_DASHES := 3
 
 #---- EXPORTS -----
 # export(int) var EXPORT_NAME # Optionnal comment
@@ -19,10 +20,12 @@ const DASH_VELOCITY := 1500
 # var public_var # Optionnal comment
 
 #==== PRIVATE ====
-# var _private_var # Optionnal comment
+var _dashes_available := MAX_DASHES
 
 #==== ONREADY ====
-# onready var onready_var # Optionnal comment
+@onready var onready_paths := {
+	"reload_timer":$"ReloadDashTimer"
+}
 
 ##### PROCESSING #####
 # Called when the object is initialized.
@@ -35,8 +38,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame. Remove the "_" to use it.
 func _process(_delta):
-	if ActionHandlerBase.is_just_active(state):
+	if ActionHandlerBase.is_just_active(state) and _dashes_available > 0:
 		player.velocity = player.direction.normalized() * DASH_VELOCITY
+		_dashes_available -= 1
+		if onready_paths.reload_timer.is_stopped():
+			onready_paths.reload_timer.start()
 
 ##### PUBLIC METHODS #####
 # Methods that are intended to be "visible" to other nodes or scripts
@@ -44,10 +50,11 @@ func _process(_delta):
 #     pass
 
 ##### PROTECTED METHODS #####
-# Methods that are intended to be used exclusively by this scripts
-# func _private_method(arg):
-#     pass
+func _print_dashes_available() -> void:
+	print("dashes available = %d" % _dashes_available)
 
 ##### SIGNAL MANAGEMENT #####
-# Functions that should be triggered when a specific signal is received
-
+func _on_reload_dash_timer_timeout():
+	_dashes_available += 1
+	if _dashes_available < MAX_DASHES:
+		onready_paths.reload_timer.start()
