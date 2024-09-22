9ac430282c025c7f50048158e7a02b0f50d9ea99
clemetayer <c.lemetayer@laposte.net>
2024-09-22 18:16:59 +0200

    added a texture for the destructible wall

---
+++ Scenes/DestructibleWalls/moving_pattern.gd
@@ -0,0 +1,56 @@
+@tool
+extends ColorRect
+# utility script to automatically control the size of the moving pattern 
+
+##### SIGNALS #####
+# Node signals
+
+##### ENUMS #####
+# enumerations
+
+##### VARIABLES #####
+#---- CONSTANTS -----
+# const constant := 10 # Optionnal comment
+
+#---- EXPORTS -----
+
+#---- STANDARD -----
+#==== PUBLIC ====
+# var public_var # Optionnal comment
+
+#==== PRIVATE ====
+# var _private_var # Optionnal comment
+
+#==== ONREADY ====
+@onready var tilemap := $".."
+
+##### PROCESSING #####
+# Called when the object is initialized.
+func _init():
+	pass
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame. Remove the "_" to use it.
+func _process(_delta):
+	if(Engine.is_editor_hint()):
+		var tilemap_rect = tilemap.get_used_rect()
+		# print("used rect = %s" % tilemap_rect)
+		size = tilemap_rect.size * 64
+		position = -tilemap_rect.size * 64 / 2
+		material.set_shader_parameter("fract_size",tilemap_rect.size)
+
+##### PUBLIC METHODS #####
+# Methods that are intended to be "visible" to other nodes or scripts
+# func public_method(arg : int) -> void:
+#     pass
+
+##### PROTECTED METHODS #####
+# Methods that are intended to be used exclusively by this scripts
+# func _private_method(arg):
+#     pass
+
+##### SIGNAL MANAGEMENT #####
+# Functions that should be triggered when a specific signal is received
