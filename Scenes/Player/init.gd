9eca5dee03b8908cd8db085fd2b1f9bd58c99f93
clemetayer <clemetayer@proton.me>
2024-11-20 21:16:10 +0100

    fixed some multiplayer issues

--- Scenes/Player/init.gd
+++ Scenes/Player/init.gd
@@ -36,6 +36,7 @@ func initialize(config : PlayerConfig) -> void:
 		PRIMARY_WEAPON = config.PRIMARY_WEAPON
 		MOVEMENT_BONUS_HANDLER = config.MOVEMENT_BONUS_HANDLER
 		POWERUP_HANDLER = config.POWERUP_HANDLER
+		onready_paths_node.crosshair.set_color(config.SPRITE_CUSTOMIZATION.BODY_COLOR)
 	onready_paths_node.primary_weapon = StaticPrimaryWeaponHandler.get_weapon(PRIMARY_WEAPON)
 	onready_paths_node.movement_bonus = StaticMovementBonusHandler.get_handler(MOVEMENT_BONUS_HANDLER)
 	onready_paths_node.powerup_manager = StaticPowerupHandler.get_powerup_manager(POWERUP_HANDLER)
@@ -46,7 +47,6 @@ func initialize(config : PlayerConfig) -> void:
 	onready_paths_node.player_root.add_child(onready_paths_node.primary_weapon)
 	onready_paths_node.player_root.add_child(onready_paths_node.movement_bonus)
 	onready_paths_node.player_root.add_child(onready_paths_node.powerup_manager)
-	onready_paths_node.crosshair.set_color(config.SPRITE_CUSTOMIZATION.BODY_COLOR)
 
 ##### PROTECTED METHODS #####
 # Methods that are intended to be used exclusively by this scripts
