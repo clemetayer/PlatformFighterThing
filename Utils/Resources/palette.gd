944946d691e251787b403a90a784081133cc731d
clemetayer <clemetayer@proton.me>
2025-01-12 18:01:42 +0100

    added a first version of a level background

---
+++ Utils/Resources/palette.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name Palette
+# Simple resource to save color palettes easily
+
+##### VARIABLES #####
+#---- EXPORTS -----
+@export var PALETTE : Array[PaletteCategory]
---
+++ Utils/Resources/palette_category.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name PaletteCategory
+# Category for a palette, to organize colors
+
+##### VARIABLES #####
+#---- EXPORTS -----
+@export var CATEGORY : Array[Color]
