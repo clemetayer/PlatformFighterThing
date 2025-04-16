ee3f5c3f91e3930cdcbc865bd9805bed0df3f83f
clemetayer <clemetayer@proton.me>
2025-04-16 09:00:19 +0200

    added a basic player data ui

--- Scenes/Player/rich_text_hit.gd
+++ Scenes/Player/rich_text_hit.gd
@@ -9,17 +9,17 @@ var bbcode = "hit"
 
 func _process_custom_fx(char_fx):
 	# Get parameters, or use the provided default value if missing.
-    var amplitude = char_fx.env.get("amplitude", 50.0)
-    var progress = char_fx.env.get("progress", 1.0)
-    var char_seed = char_fx.env.get("seed",1)
+	var amplitude = char_fx.env.get("amplitude", 50.0)
+	var progress = char_fx.env.get("progress", 1.0)
+	var char_seed = char_fx.env.get("seed",1)
 
-    var rngx = RandomNumberGenerator.new()
-    rngx.seed = (char_fx.relative_index + 1) * char_seed
-    var randx = (0.5 - rngx.randf()) * 2.0 * float(amplitude)
-    var rngy = RandomNumberGenerator.new()
-    rngy.seed = (char_fx.relative_index + 1) * char_seed
-    var randy = (0.5 - rngy.randf()) * 2.0 * float(amplitude)
-    var rand_offset = Vector2(randx, randy)
+	var rngx = RandomNumberGenerator.new()
+	rngx.seed = (char_fx.relative_index + 1) * char_seed
+	var randx = (0.5 - rngx.randf()) * 2.0 * float(amplitude)
+	var rngy = RandomNumberGenerator.new()
+	rngy.seed = (char_fx.relative_index + 1) * char_seed
+	var randy = (0.5 - rngy.randf()) * 2.0 * float(amplitude)
+	var rand_offset = Vector2(randx, randy)
 
-    char_fx.offset = rand_offset * (1.0 - progress)
-    return true
+	char_fx.offset = rand_offset * (1.0 - progress)
+	return true
